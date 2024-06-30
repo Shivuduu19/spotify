16a37dbe57eee48d60a98c0da3079dfad9728f1c
shiva kumar <98257507+Shivuduu19@users.noreply.github.com.com>
2024-06-30 13:16:14 +0530

    correct ordering

--- src/components/SliderCard.jsx
+++ src/components/SliderCard.jsx
@@ -3,19 +3,19 @@ import React from 'react'
 const SliderCard = ({ branch, role, speciality, indexed, current, location, width }) => {
     return (
         <div style={{ width }} className={`flex flex-shrink-0 mr-[20px] h-auto  `}>
-            <a href="" className={`${indexed + 1 === current ? 'bg-[#96f0b6] text-[#1914144d]' : ''} border cursor-pointer w-full mx-auto my-0 transition-[border-color,background-color] duration-[.4s] p-[30px] min-h-[500px] flex flex-col max-w-[413px] flex-shrink-0 rounded-[20px] bg-transparent   `}>
+            <a href="" className={` border cursor-pointer w-full mx-auto my-0 transition-[border-color,background-color] duration-[.4s] p-[30px] min-h-[500px] flex flex-col max-w-[413px] flex-shrink-0 rounded-[20px] bg-transparent ${indexed + 1 === current ? 'bg-[#96f0b6] text-[#1914144d]' : ''}  `}>
                 <div className='flex justify-center '>
-                    <div className={`${indexed + 1 === current ? 'bg-[#ffffff4d]' : ''} bg-[#00000033] px-[15px]  rounded-[100px] flex items-center min-h-[40px] `}>
-                        <p className={`${indexed + 1 === current ? 'text-[#191414]' : ''} text-[#fff] font-normal text-[calc(1rem+.125*(100vw-48rem)/42)] m-0`}>{branch}</p>
+                    <div className={` bg-[#00000033] ${indexed + 1 === current ? 'bg-[#ffffff4d]' : ''} px-[15px]  rounded-[100px] flex items-center min-h-[40px] `}>
+                        <p className={`text-[#fff] ${indexed + 1 === current ? 'text-[#191414]' : ''}  font-normal text-[calc(1rem+.125*(100vw-48rem)/42)] m-0`}>{branch}</p>
                     </div>
                 </div>
                 <p className='transition-colors duration-[.4s] flex-grow-[1] break-words mb-[35px]  text-center leading-[120%] font-normal text-[calc(1.375rem+.875*(100vw-48rem)/42)] mt-[40px] m-0 text-[#1914144d]'>
-                    <span className={`${indexed + 1 === current ? 'text-[#191414]' : ''} text-[#fff] `}>{role}</span>
+                    <span className={` text-[#fff] ${indexed + 1 === current ? 'text-[#191414]' : ''} `}>{role}</span>
                     <br />
                     {speciality}
                 </p>
                 <div className=''>
-                    <p className={`${indexed + 1 === current ? 'text-[#191414]' : ''} text-[#fff] text-[calc(1rem+.125*(100vw-48rem)/42)] text-center font-normal m-0`}>{location}</p>
+                    <p className={` text-[#fff] ${indexed + 1 === current ? 'text-[#191414]' : ''} text-[calc(1rem+.125*(100vw-48rem)/42)] text-center font-normal m-0`}>{location}</p>
                 </div>
             </a>
         </div>
